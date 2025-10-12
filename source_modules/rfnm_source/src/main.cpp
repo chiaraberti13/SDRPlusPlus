8277993639c40ab7ad4104a92269dcfa2633dca2
Satoshi Brasileiro <102581645+chiaraberti13@users.noreply.github.com>
2025-10-12 23:06:29 +0200

    Adjust RFNM conversion scaling

--- source_modules/rfnm_source/src/main.cpp
+++ source_modules/rfnm_source/src/main.cpp
@@ -495,8 +495,8 @@ private:
             // We'll reuse SDR++ ring policy: accumulate then swap.
             size_t sampCount = elems_read;
             // Ensure we have enough room; SDR++ stream will manage swap cadence
-            // CORREZIONE: manteniamo il fattore di conversione CORRETTO (1.0f / 32768.0f)
-            volk_16i_s32f_convert_32f((float*)&stream.writeBuf[0], tmpIQ.data(), 1.0f / 32768.0f, sampCount * 2);
+            // CORREZIONE: manteniamo il fattore di conversione CORRETTO (32768.0f)
+            volk_16i_s32f_convert_32f((float*)&stream.writeBuf[0], tmpIQ.data(), 32768.0f, sampCount * 2);
 
             if (!stream.swap((int)sampCount)) {
                 // Stop requested or downstream closed
